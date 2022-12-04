3965ee1c232dceab8cb43206297fbfedb26d9754
nimaguerrero <nima_guerrero@hotmail.com>
2022-12-04 18:19:17 -0500

    configuracion en local y desarrollo

--- core/env.php
+++ core/env.php
@@ -1,37 +1,32 @@
 <?php
-
-// define('DATABASE', [
-//     'driver'    => 'mysql',
-//     'host'      => 'localhost',
-//     'database'  => 'vacunas',
-//     'username'  => 'root',
-//     'password'  => '',
-//     'charset'   => 'utf8',
-//     'collation' => 'utf8_unicode_ci',
-//     'prefix'    => ''
-// ]);
-
 session_start();
 
-$_ENV["ENT"] = "";
+if (!empty($_ENV["ENT"]) && $_ENV["ENT"] == "desarrollo") {
 
-if (empty($_ENV["ENT"])) {
-    echo("ESTAS en Local");
+    define('DATABASE', [
+        'driver'    => 'mysql',
+        'host'      => $_ENV["DB_HOST"],
+        'database'  => $_ENV["DB_NAME"],
+        'username'  => $_ENV["DB_USER"],
+        'password'  => $_ENV["DB_PASSWORD"],
+        'charset'   => $_ENV["DB_CHARSET"],
+        'port'      => $_ENV["DB_PORT"],
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => ''
+    ]);
+    
 } else {
-    echo("Estas en desarrollo");
+    
+    define('DATABASE', [
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'vacunas',
+        'username'  => 'root',
+        'password'  => '',
+        'charset'   => 'utf8mb4',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => ''
+    ]);
 }
-// define('DATABASE', [
-//     'driver'    => 'mysql',
-//     'host'      => $_ENV["DB_HOST"],
-//     'database'  => $_ENV["DB_NAME"],
-//     'username'  => $_ENV["DB_USER"],
-//     'password'  => $_ENV["DB_PASSWORD"],
-//     'charset'   => $_ENV["DB_CHARSET"],
-//     'port'      => $_ENV["DB_PORT"],
-//     'collation' => 'utf8_unicode_ci',
-//     'prefix'    => ''
-// ]);
-
-
 
 ?>
