b20dd8ad7b81f401300102b1a8e12920aae865d8
nimaguerrero <nima_guerrero@hotmail.com>
2022-12-04 17:24:10 -0500

    usando el ENTORNO

--- core/env.php
+++ core/env.php
@@ -13,17 +13,24 @@
 
 session_start();
 
-define('DATABASE', [
-    'driver'    => 'mysql',
-    'host'      => $_ENV["DB_HOST"],
-    'database'  => $_ENV["DB_NAME"],
-    'username'  => $_ENV["DB_USER"],
-    'password'  => $_ENV["DB_PASSWORD"],
-    'charset'   => $_ENV["DB_CHARSET"],
-    'port'      => $_ENV["DB_PORT"],
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => ''
-]);
+$_ENV["ENT"] = "";
+
+if (empty($_ENV["ENT"])) {
+    echo("ESTAS en Local");
+} else {
+    echo("Estas en desarrollo");
+}
+// define('DATABASE', [
+//     'driver'    => 'mysql',
+//     'host'      => $_ENV["DB_HOST"],
+//     'database'  => $_ENV["DB_NAME"],
+//     'username'  => $_ENV["DB_USER"],
+//     'password'  => $_ENV["DB_PASSWORD"],
+//     'charset'   => $_ENV["DB_CHARSET"],
+//     'port'      => $_ENV["DB_PORT"],
+//     'collation' => 'utf8_unicode_ci',
+//     'prefix'    => ''
+// ]);
 
 
 
